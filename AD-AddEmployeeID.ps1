e069d6e3d5747c23d6decae29021914f701271e1
ferus-wylde~ <feruswylde@gmail.com>
2020-12-30 21:43:48 +0000

    Exit code added

---
+++ AD-AddEmployeeID.ps1
@@ -0,0 +1,34 @@
+#   James Wylde 2019
+#   ferus-wylde@github
+
+# Modules
+Import-Module ActiveDirectory
+
+Write-Host - '******************************************************************' -ForegroundColor White -BackgroundColor Red
+Write-Host - '******************************************************************' -ForegroundColor White -BackgroundColor Red
+Write-Host - '**ENSURE RELEVANT PERMISSIONS HAVE BEEN SOUGHT BEFORE PROCEEDING**' -ForegroundColor White -BackgroundColor Red
+Write-Host - '******************************************************************' -ForegroundColor White -BackgroundColor Red
+Write-Host - '******************************************************************' -ForegroundColor White -BackgroundColor Red
+Read-Host -Prompt "Press ENTER to continue..."
+
+$SearchUser = Read-Host -Prompt 'Search user'
+    $Users = Get-Aduser -Filter "anr -like '$SearchUser'"
+    if ($Users.count -gt 1){
+        Write-Host 'Multiple users were found.' -ForegroundColor Red -BackgroundColor Yellow
+        Write-Host 'Please select a user:' -ForegroundColor Red -BackgroundColor Yellow
+        for($i = 0; $i -lt $Users.count; $i++){
+            Write-Host "$($i): $($Users[$i].SamAccountName) | $($Users[$i].Name)"
+        }
+        $selection = Read-Host -Prompt 'Select user (0, 1, 2 etc.)'
+        $ResultUser = $Users[$selection]
+    }
+
+$EID = Read-Host -Prompt 'New employee number:'
+    if ($EID) { 
+        Write-Host = "Employee ID change successful." -ForegroundColor White -BackgroundColor Green
+    } 
+    else { 
+        Write-Host = "Employee ID change unsuccessful." -ForegroundColor White -BackgroundColor Red
+    }
+
+Set-ADUser $ResultUser -EmployeeID $EID
