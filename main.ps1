ff5f7f19989a07fb6fe375e2bc5116cece98d623
jameswylde <feruswylde@gmail.com>
2021-01-14 20:21:46 +0000

    Upd

--- main.ps1
+++ main.ps1
@@ -5,18 +5,22 @@ $mainForm = New-Object System.Windows.Forms.Form
 $textBox = New-Object System.Windows.Forms.TextBox
 $daResolver = New-Object System.Windows.Forms.Button
 $daReboot = New-Object System.Windows.Forms.Button
+$daDiagnostics = New-Object System.Windows.Forms.Button
+
+# Functions
+
 
 #   GUI Textbox Out
-$textBox.Location = '23,10'
+$textBox.Location = '20,10'
 $textBox.Size = '450,350'
 $textBox.ReadOnly = $true
 
 #   Button (L) DirectAccess Service Stop & Start
-$daResolver.Text = 'DirectAccess Resolver'
-$daResolver.Size = '175,23'
-$daResolver.Location = '56,220'
+$daResolver.Text = 'Resolver'
+$daResolver.Size = '140,23'
+$daResolver.Location = '175,220'
 $daResolver.Add_Click({
-
+    
     Stop-Service iphlpsvc -Force
     Stop-Service NcaSvc
 
@@ -24,42 +28,61 @@ $daResolver.Add_Click({
 
     Start-Service iphlpsvc
     Start-Service NcaSvc
+
+    $textBox.Text = Write-Output "DirectAccess is now restarting..."
+    Start-Sleep -Seconds 25
+    Clear-Host
+
+    $textBox.Text = Write-Output "DirectAccess is now RUNNING. You can now close this application."
 })
 
 #   Button (R) DirectAccess Service Stop & Start, IPV6 release and reboot
-$daReboot.Size = '175,23'
-$daReboot.Location = '250,220'
-$daReboot.text = 'DirectAccess Resolver + Reboot'
+$daReboot.Size = '140,23'
+$daReboot.Location = '330,220'
+$daReboot.text = 'Resolver + Reboot'
 $daReboot.Add_Click({
 
     Stop-Service iphlpsvc -Force
     Stop-Service NcaSvc
 
-    Start-Sleep -Seconds 2
+    Start-Sleep -Seconds 3
 
     Start-Service iphlpsvc
     Start-Service NcaSvc
 
-    Start-Sleep -Seconds 2
+    Start-Sleep -Seconds 3
 
     ipconfig.exe /release6
     ipconfig.exe /renew
 
-    Stop-Computer -Confirm
+    $textBox.Text = Write-Output "DirectAccess is now restarting and IPV6 being released..."
+    Start-Sleep -Seconds 25
+    Clear-Host
+
+    $textBox.Text = Write-Output "Your machine will SHUTDOWN in 60 seconds. Please save all work."
+    Stop-Computer -Force
 })
 
+
+$daDiagnostics.Text = 'Diagnostics'
+$daDiagnostics.Size = '140,23'
+$daDiagnostics.Location = '20,220'
+$daDiagnostics.Add_Click({
+
+})
 
 #   Unrequired I/O 
 #   $mainForm.AcceptButton = $okButton
 #   $mainForm.CancelButton = $cancelButton
 
 $mainForm.Text = 'Direct Access Resolver'
-$mainForm.Size = "500,300" 
+$mainForm.Size = "505,300" 
 
 
 $mainForm.Controls.Add($textBox)
 $mainForm.Controls.Add($daResolver)
 $mainForm.Controls.Add($daReboot)
+$mainForm.Controls.Add($daDiagnostics)
 
 
 $mainForm.ShowDialog()
