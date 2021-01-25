2deff16daa466c6763120dcb763bbffc7255b152
ferus-wylde~ <feruswylde@gmail.com>
2021-01-25 11:30:01 +0000

    Update

--- AD-ExportGroupCSV.ps1
+++ AD-ExportGroupCSV.ps1
@@ -4,5 +4,4 @@
 #   Modules
 Import-Module ActiveDirectory
 
-test
-}
+Get-ADUser -Properties * -Filter {Enabled -eq 'True'} | Where-Object {($_.memberof -like "_UK DSS ALL")} | Select-Object @{Label = "Email";Expression = {$_.EmailAddress}}, @{Label = "First Name";Expression = {$_.GivenName}}, @{Label = "Last Name";Expression = {$_.Surname}}, @{Label = "Group";Expression = {($_.canonicalname -Split "/")[-2]}} | Export-Csv -Path c:\temp\export2.csv -NoTypeInformation
--- MISC-DeleteOST.ps1
+++ MISC-DeleteOST.ps1
@@ -2,7 +2,7 @@
 #   github.com/jameswylde
 
 #   Modules
-
+#.
 #   Notes
 #   Created for large number of users experiencing 'Unable to launch Outlook' - much faster than remoting on, creating new profile, booting into Outlook SM etc. 
 #   Closes Outlook, creates backup of current .OST in their Documents, and re-opens Outlook forcing user to create new profile and having new .OST generate
