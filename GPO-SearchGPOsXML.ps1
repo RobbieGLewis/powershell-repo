53b569ead64870c28335409102437bbebca69d59
jameswylde <feruswylde@gmail.com>
2021-02-11 13:59:33 +0000

    Upd

--- GPO-SearchGPOsXML.ps1
+++ GPO-SearchGPOsXML.ps1
@@ -1,29 +1,54 @@
-# Get the string we want to search for 
-$string = Read-Host -Prompt "String" 
- 
-# Set the domain to search for GPOs 
+
+#----------------------------------------------------------------------------------------#
+
+#   Imports
+
+Import-Module GroupPolicy 
+
+#----------------------------------------------------------------------------------------#
+
+#   VARS
+
 $DomainName = $env:USERDNSDOMAIN 
+
+#$FileOut = 'C:\Temp\Exports\GPOsearch.csv'
+
+#----------------------------------------------------------------------------------------#
+
+#   Search
+
+$string = Read-Host -Prompt "String" 
+
+
  
-# Find all GPOs in the current domain 
-write-host "Finding all the GPOs in $DomainName" 
-Import-Module grouppolicy 
+#----------------------------------------------------------------------------------------#
+
+#   GPOs in Domain
+
+Write-Host "Searching Group Policy Objects in $DomainName" 
 $allGposInDomain = Get-GPO -All -Domain $DomainName 
 [string[]] $MatchedGPOList = @()
 
-# Look through each GPO's XML for the string 
+#----------------------------------------------------------------------------------------#
+
+#   GPO XML search
+
 Write-Host "Starting search...." 
 foreach ($gpo in $allGposInDomain) { 
     $report = Get-GPOReport -Guid $gpo.Id -ReportType Xml 
     if ($report -match $string) { 
-        write-host "********** Match found in: $($gpo.DisplayName) **********" -foregroundcolor "Green"
+        Write-Host "****************** Match found in: $($gpo.DisplayName) ******************" -foregroundcolor "Green"
         $MatchedGPOList += "$($gpo.DisplayName)";
-    } # end if 
+    } 
     else { 
-        Write-Host "No match in: $($gpo.DisplayName)" 
-    } # end else 
-} # end foreach
-write-host "`r`n"
-write-host "Results: **************" -foregroundcolor "Yellow"
+        Write-Host "No match in: $($gpo.DisplayName)" -foregroundcolor "Red"
+    } 
+} 
+Write-Host " `r`n "
+Write-Host "Results: **************" -foregroundcolor "Yellow"
 foreach ($match in $MatchedGPOList) { 
-    write-host "Match found in: $($match)" -foregroundcolor "Green"
+    Write-Host "Match found in: $($match)" -foregroundcolor "Green"
+
+#Write-Output $Output | Out-File $FileOut -Encoding utf8 -Append
 }
+
