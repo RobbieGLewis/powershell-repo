6d9613c4450ccd86a6ea0da596486dfe212c801d
ferus-wylde~ <feruswylde@gmail.com>
2021-02-16 21:55:58 +0000

    Upd

--- WS-ServerChecker.ps1
+++ WS-ServerChecker.ps1
@@ -1,30 +1,35 @@
-$serverName = Read-Host -Prompt "Server: "
+$serverName = Read-Host -Prompt "Server Bouncer: "
 
 function Show-Menu
 {
      param (
-           [string]$Title = '$serverName'
+           [string]$Title = $serverName
      )
      Clear-Host
      Write-Host "================ $Title ================"
 
-     Write-Host "1: Press '1' to check uptime."
-     Write-Host "2: Press '2' to check CPU and MEMORY usage."
-     Write-Host "3: Press '3' to turn ambifacient lunar wane shaft."
-     Write-Host "Q: Press 'Q' to quit."
+     Write-Host "1 - Check uptime."
+     Write-Host "2 - Check CPU and MEMORY usage."
+     Write-Host "3 - Close all applications."
+     Write-Host "4 - Reboot server."
+
+     Write-Host "Q - Quit."
 }
 
 #Functions go here
 
 Function function1 {(Get-Date) - (Get-CimInstance Win32_OperatingSystem -ComputerName $serverName).LastBootupTime | Format-Table -AutoSize}
 Function function2 {ps | sort -des cpu | Select-Object -f 15 | Format-Table -Autosize; sleep 1}
-Function function3 {...}
+Function function3 {Get-Process | Where-Object {$_.MainWindowTitle -ne ""} | stop-process}
+Function function4 {
+    Read-Host -Prompt "ARE YOU SURE YOU WANT TO REBOOT $servername ?"
+    Restart-Computer -ComputerName $serverName -Force}
 
 #Main menu loop
 do
 {
      Show-Menu
-     $input = Read-Host "Please make a selection"
+     $input = ($host.UI.RawUI.ReadKey('NoEcho,IncludeKeyUp')).character
      switch ($input)
      {
            '1' {
@@ -36,6 +41,9 @@ do
            } '3' {
                 Clear-Host
                 function3
+            } '4' {
+                Clear-Host
+                function4
            } 'q' {
                 return
            }
