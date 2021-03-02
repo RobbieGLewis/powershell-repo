452e7cd02d8d5809537ba23086e5a4b237400781
ferus-wylde~ <feruswylde@gmail.com>
2021-03-02 19:50:32 +0000

    temp

---
+++ MISC-CcmUpdate.ps1
@@ -0,0 +1,6 @@
+$Path = Get-ChildItem -Path C:\Windows\CCMCache -Recurse -Filter setup1.exe
+
+ForEach ( $Installer in ( Get-ChildItem -Path $Path.DirectoryName -Filter *.exe) ) {
+
+Start-Process -Wait -FilePath setup1.exe -ArgumentList "/i $($Installer.FullName)"
+}
