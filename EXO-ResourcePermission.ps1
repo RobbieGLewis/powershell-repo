3c6cefbdc70a1ce2eae3eeb989c23793f19a3d6f
jameswylde <feruswylde@gmail.com>
2021-03-05 16:29:16 +0000

    upd

--- EXO-ResourcePermission.ps1
+++ EXO-ResourcePermission.ps1
@@ -7,3 +7,6 @@
 Add-RecipientPermission "MeetingRoom.Corby@smurfitkappa.co.uk" -trustee "Mike.Devine@smurfitkappa.co.uk" -AccessRights sendAs -Confirm:$false
 Get-RecipientPermission "MeetingRoom.Corby@smurfitkappa.co.uk"
 Add-MailboxPermission "MeetingRoom.Corby@smurfitkappa.co.uk" -user "Mike.Devine@smurfitkappa.co.uk" -AccessRights fullaccess -Confirm:$false
+
+
+Get-RecipientPermission "UKCORALL@smurfitkappa.co.uk"
