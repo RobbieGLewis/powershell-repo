e594dfec046bd821d5deed16a74ca5168dd3a6cb
ferus-wylde~ <feruswylde@gmail.com>
2021-03-18 21:26:41 +0000

    .

--- MISC-SearchToCSV.ps1
+++ MISC-SearchToCSV.ps1
@@ -6,10 +6,25 @@
 
 $var = Read-Host = "Search term"
 
-Get-ChildItem -Path C:\ -Include $var -File -Recurse -ErrorAction SilentlyContinue
+Get-ChildItem -Path C:\ -Include $var -File -Recurse -ErrorAction SilentlyContinue | Export-CSV -Path C:\Temp\export.csv
 
-$results = Get-ChildItem -Path C:\ -Include $var -File -Recurse -ErrorAction SilentlyContinue | Export-CSV -Path C:\Temp\export.csv
+#$results = Get-ChildItem -Path C:\ -Include $var -File -Recurse -ErrorAction SilentlyContinue | Export-CSV -Path C:\Temp\export.csv
 
 Write-Host = "`r`n"
 
 Write-Host = "Results are located C:\Temp\export.csv" -ForegroundColor Red -BackgroundColor Green
+
+
+#----------------------------------------------------------------------------------------#
+#   Remote
+
+Invoke-Command -ComputerName PC01 -ScriptBlock {
+
+    $var = Read-Host = "Search term"
+    
+    Get-ChildItem -Path C:\ -Include $var -File -Recurse -ErrorAction SilentlyContinue} | Export-CSV -Path C:\Temp\export.csv
+
+Write-Host = "`r`n"
+
+Write-Host = "Results are located C:\Temp\export.csv" -ForegroundColor Red -BackgroundColor Green
+    
