0dd688e00bc7745ba9f430bba387df8d86976c07
jameswylde <feruswylde@gmail.com>
2021-03-19 14:08:00 +0000

    .

--- MISC-SearchToCSV.ps1
+++ MISC-SearchToCSV.ps1
@@ -20,16 +20,18 @@ Write-Host = "Results are located C:\Temp\export.csv" -ForegroundColor Red -Back
 
 Clear-Host
 
-Invoke-Command -ComputerName PC01 -ScriptBlock {
-
+$hostname = Read-Host - "Hostname"
+ 
+Invoke-Command -ComputerName $hostName -ScriptBlock {
+ 
 $var = Read-Host = "Search"
-
-Get-ChildItem -Path C:\ -Include $var -File -Recurse -ErrorAction SilentlyContinue}
-
-Get-ChildItem -Path C:\ -Include $var -File -Recurse -ErrorAction SilentlyContinue | Export-CSV -Path C:\Temp\export.csv | Out-File -FilePath C:\Temp\export.txt
-
+ 
+Get-ChildItem -Path C:\temp -Include $var -File -Recurse -ErrorAction SilentlyContinue}
+ 
+Get-ChildItem -Path C:\temp -Include $var -File -Recurse -ErrorAction SilentlyContinue | Export-CSV -Path C:\Temp\export.csv | Out-File -FilePath C:\Temp\export.txt
+ 
 #Get-ChildItem -Path C:\ -Include $var -File -Recurse -ErrorAction SilentlyContinue | Out-File -FilePath C:\Temp\export.txt
-
+ 
 Write-Host = "`r`n"
-
-Write-Host = "Results are located C:\Temp\export.csv" -ForegroundColor Green -BackgroundColor White
+ 
+Write-Host = "Results are located C:\Temp\export.csv" -ForegroundColor Black -BackgroundColor White
