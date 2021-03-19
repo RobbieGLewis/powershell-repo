09a4e37c3fca6663d879cef9469270871e81e1a3
ferus-wylde~ <feruswylde@gmail.com>
2021-03-19 22:53:52 +0000

    .

---
+++ MISC-TeamsRDP.ps1
@@ -0,0 +1,11 @@
+
+$clientName = Read-Host 'Machine'
+$userName = Read-Host 'User' 
+psexec.exe \\$clientName cmd /C "net start winrm & netsh firewall set service type = remotedesktop mode = enable & reg.exe add "HKLM\SYSTEM\CurrentControlSet\Control\Terminal Server" /v fDenyTSConnections /t REG_DWORD /d 0 /f & reg.exe add "HKLM\Software\Microsoft\Windows NT\Current Version\AppCompatFlags\Layers" /v "C:\Users\$username\AppData\Local\Microsoft\Teams\current\teams.exe" /T REG_SZ /d "WIN7RTM" /F & reg.exe Add "HKLM\Software\Microsoft\Windows NT\Current Version\AppCompatFlags\Layers" /v "C:\ProgramData\$userName\Microsoft\Teams\Update.exe" /T REG_SZ /d "WIN7RTM" /F & reg.exe Add "HKLM\Software\Microsoft\Windows NT\Current Version\AppCompatFlags\Layers" /v "C:\Program Files\Internet Explorer\iexplore.exe" /T REG_SZ /d "WIN7RTM" /F"
+
+
+
+
+
+
+#paexec.exe \\PC01 -s c:\windows\system32\winrm.cmd quickconfig -quiet
