9ab7bffc9a96e6ef95fe213450cb737e7d11eea5
jameswylde <feruswylde@gmail.com>
2021-03-22 09:10:28 +0000

    .

--- MISC-TeamsRDP.ps1
+++ MISC-TeamsRDP.ps1
@@ -57,7 +57,7 @@ Start-Sleep -Seconds 10
 
 #----------------------------------------------------------------------------------------#
 #   For work domain   [ need to add /reg:64 switch to bypass reg being added to WOW6432NODE]   
-
+#   Using: netsh taskkill.exe reg.exe 
 
 Clear-Host
 
@@ -69,7 +69,7 @@ Write-Host = "`r`n"
 $clientName = Read-Host 'Machine'
 $userName = Read-Host 'User' 
 
-C:\temp\TeamsRDPfix\tools\paexec.exe \\$clientName cmd /C "net start winrm & netsh firewall set service type = remotedesktop mode = enable & netsh advfirewall firewall add rule name="Open Remote Desktop" protocol=TCP dir=in localport=3389 action=allow & reg.exe add "HKLM\SYSTEM\CurrentControlSet\Control\Terminal Server" /v fDenyTSConnections /t REG_DWORD /d 0 /f & taskkill /im TEAMS.EXE /f & reg.exe add "HKLM\Software\Microsoft\Windows NT\Current Version\AppCompatFlags\Layers" /v "C:\Users\$username\AppData\Local\Microsoft\Teams\current\teams.exe" /T REG_SZ /d "WIN7RTM" /F /reg:64 & reg.exe add "HKLM\Software\Microsoft\Windows NT\Current Version\AppCompatFlags\Layers" /v "C:\ProgramData\$userName\Microsoft\Teams\Update.exe" /T REG_SZ /d "WIN7RTM" /F /reg:64 & reg.exe add "HKLM\Software\Microsoft\Windows NT\CurrentVersion\AppCompatFlags\Layers" /v "C:\Users\$username\AppData\Local\Microsoft\Teams\current\teams.exe" /T REG_SZ /d "WIN7RTM" /F /reg:64 & reg.exe Add "HKLM\Software\Microsoft\Windows NT\CurrentVersion\AppCompatFlags\Layers" /v "C:\ProgramData\$userName\Microsoft\Teams\Update.exe" /T REG_SZ /d "WIN7RTM" /F /reg:64 & reg.exe add "HKLM\Software\Microsoft\Windows NT\Current Version\AppCompatFlags\Layers" /v "C:\ProgramData\$userName\Microsoft\Teams\Update.exe" /T REG_SZ /d "WIN7RTM" /F /reg:64" | Out-File -FilePath C:\Temp\TeamsRDPfix\result.txt
+C:\temp\TeamsRDPfix\tools\paexec.exe \\$clientName cmd /C "net start winrm & netsh firewall set service type = remotedesktop mode = enable & netsh advfirewall firewall add rule name="Open Remote Desktop" protocol=TCP dir=in localport=3389 action=allow & reg.exe add "HKLM\SYSTEM\CurrentControlSet\Control\Terminal Server" /v fDenyTSConnections /t REG_DWORD /d 0 /f & taskkill /im TEAMS.EXE /f & reg.exe add "HKLM\Software\Microsoft\Windows NT\Current Version\AppCompatFlags\Layers" /v "C:\Users\$username\AppData\Local\Microsoft\Teams\current\teams.exe" /T REG_SZ /d "WIN7RTM" /F /reg:64 & reg.exe add "HKLM\Software\Microsoft\Windows NT\Current Version\AppCompatFlags\Layers" /v "C:\ProgramData\$userName\Microsoft\Teams\Update.exe" /T REG_SZ /d "WIN7RTM" /F /reg:64 & reg.exe add "HKLM\Software\Microsoft\Windows NT\CurrentVersion\AppCompatFlags\Layers" /v "C:\Users\$username\AppData\Local\Microsoft\Teams\current\teams.exe" /T REG_SZ /d "WIN7RTM" /F /reg:64 & reg.exe Add "HKLM\Software\Microsoft\Windows NT\CurrentVersion\AppCompatFlags\Layers" /v "C:\ProgramData\$userName\Microsoft\Teams\Update.exe" /T REG_SZ /d "WIN7RTM" /F /reg:64 & reg.exe add "HKLM\Software\Microsoft\Windows NT\Current Version\AppCompatFlags\Layers" /v "C:\ProgramData\$userName\Microsoft\Teams\Update.exe" /T REG_SZ /d "WIN7RTM" /F /reg:64 & reg.exe Add "HKLM\Software\Microsoft\Windows NT\CurrentVersion\AppCompatFlags\Layers" /v C:\Users\$username\AppData\Local\Microsoft\Teams\update.exe /T REG_SZ /d "WIN7RTM" /F /reg:64 & reg.exe Add "HKLM\Software\Microsoft\Windows NT\Current Version\AppCompatFlags\Layers" /v C:\Users\$username\AppData\Local\Microsoft\Teams\update.exe /T REG_SZ /d "WIN7RTM" /F /reg:64" | Out-File -FilePath C:\Temp\TeamsRDPfix\result.txt
 
 Get-Content -Path C:\temp\TeamsRDPfix\result.txt
 
