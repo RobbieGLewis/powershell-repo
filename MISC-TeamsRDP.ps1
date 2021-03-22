e8060635b7835ab69170346f8f6cc6a79240a92f
jameswylde <feruswylde@gmail.com>
2021-03-22 13:36:05 +0000

    .

--- MISC-TeamsRDP.ps1
+++ MISC-TeamsRDP.ps1
@@ -84,3 +84,33 @@ Set-Location C:\temp\TeamsRDPfix
 Start-Sleep -Seconds 10
 
 
+
+#----------------------------------------------------------------------------------------#
+
+#   remove cache
+
+
+Clear-Host
+
+$ErrorActionPreference = 'silentlycontinue'
+
+Write-Host "`r`n"
+Write-Host "Enable RDP. Start Winrm. Create firewall exception. Apply Teams compatability regkeys."
+Write-Host "`r`n"
+
+$clientName = Read-Host 'Machine'
+$userName = Read-Host 'User' 
+
+C:\temp\TeamsRDPfix\tools\paexec.exe \\$clientName cmd /C "net start winrm & netsh firewall set service type = remotedesktop mode = enable & netsh advfirewall firewall add rule name="Open Remote Desktop" protocol=TCP dir=in localport=3389 action=allow & reg.exe add "HKLM\SYSTEM\CurrentControlSet\Control\Terminal Server" /v fDenyTSConnections /t REG_DWORD /d 0 /f & taskkill /im TEAMS.EXE /f & del /S /f /q C:\Users\$userName\AppData\Roaming\Microsoft\Teams\Cache\* &reg.exe add "HKLM\Software\Microsoft\Windows NT\Current Version\AppCompatFlags\Layers" /v "C:\Users\$username\AppData\Local\Microsoft\Teams\current\teams.exe" /T REG_SZ /d "WIN7RTM" /F /reg:64 & reg.exe add "HKLM\Software\Microsoft\Windows NT\Current Version\AppCompatFlags\Layers" /v "C:\ProgramData\$userName\Microsoft\Teams\Update.exe" /T REG_SZ /d "WIN7RTM" /F /reg:64 & reg.exe add "HKLM\Software\Microsoft\Windows NT\CurrentVersion\AppCompatFlags\Layers" /v "C:\Users\$username\AppData\Local\Microsoft\Teams\current\teams.exe" /T REG_SZ /d "WIN7RTM" /F /reg:64 & reg.exe Add "HKLM\Software\Microsoft\Windows NT\CurrentVersion\AppCompatFlags\Layers" /v "C:\ProgramData\$userName\Microsoft\Teams\Update.exe" /T REG_SZ /d "WIN7RTM" /F /reg:64 & reg.exe add "HKLM\Software\Microsoft\Windows NT\Current Version\AppCompatFlags\Layers" /v "C:\ProgramData\$userName\Microsoft\Teams\Update.exe" /T REG_SZ /d "WIN7RTM" /F /reg:64 & reg.exe Add "HKLM\Software\Microsoft\Windows NT\CurrentVersion\AppCompatFlags\Layers" /v C:\Users\$username\AppData\Local\Microsoft\Teams\update.exe /T REG_SZ /d "WIN7RTM" /F /reg:64 & reg.exe Add "HKLM\Software\Microsoft\Windows NT\Current Version\AppCompatFlags\Layers" /v C:\Users\$username\AppData\Local\Microsoft\Teams\update.exe /T REG_SZ /d "WIN7RTM" /F /reg:64" | Out-File -FilePath C:\Temp\TeamsRDPfix\result.txt
+
+Get-Content -Path C:\temp\TeamsRDPfix\result.txt
+
+Write-Host "`r`n"
+Write-Host "Finished and closing - transcript located in \TeamsRDPfix\" -ForegroundColor Blue -BackgroundColor green
+
+Set-Location C:\temp\TeamsRDPfix
+
+Start-Sleep -Seconds 10
+
+# del /S /f /q C:\Users\$userName\AppData\Roaming\Microsoft\Teams\Cache\*
+
