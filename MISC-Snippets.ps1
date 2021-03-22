e0b89200190ea86ca1aa64a943dbb99f07568572
jameswylde <feruswylde@gmail.com>
2021-03-22 17:32:05 +0000

    .

--- MISC-Snippets.ps1
+++ MISC-Snippets.ps1
@@ -8,3 +8,15 @@ do {
 
 
 netsh advfirewall monitor show consec verbose > c:\temp\output.txt
+netsh advfirewall monitor show currentprofile
+netsh advfirewall set allprofiles state off
+
+netsh firewall set opmode mode=DISABLE
+
+
+
+
+Computer\HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\Netlogon\Parameters\NegativeCachePeriod    created and changed to 0
+NLASVc - automatic delayed restart resolved
+Toggled on/off DNS on adapter
+Flushed and registered DNS
