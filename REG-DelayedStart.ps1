9ee5ef3f7f781628aed61f69cf1086116eb9e4bf
jameswylde <feruswylde@gmail.com>
2021-03-23 17:21:56 +0000

    .

---
+++ REG-DelayedStart.ps1
@@ -0,0 +1,12 @@
+Clear-Host
+
+$ErrorActionPreference = 'silentlycontinue'
+
+Write-Host "`r`n"
+Write-Host "Enable RDP. Start Winrm. Create firewall exception. Apply Teams compatability regkeys."
+Write-Host "`r`n"
+
+
+$clientName = Read-Host 'Machine'
+
+C:\temp\TeamsRDPfix\tools\paexec.exe \\$clientName cmd /C "reg.exe Add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\NlaSvc" /v DelayedAutoStart /T REG_SZ /d 1 /F & reg.exe Add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\NlaSvc" /v Start /T REG_SZ /d 2 /F"
