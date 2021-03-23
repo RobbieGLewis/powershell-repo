3c279f56e65d2cbba36fd204c2657ede0227e0f2
jameswylde <feruswylde@gmail.com>
2021-03-23 19:16:07 +0000

    .

--- MISC-Snippets.ps1
+++ MISC-Snippets.ps1
@@ -180,3 +180,19 @@ foreach($computer in Get-Content -Path c:\temp\computers1.txt){
 }
 
 # | Out-File -FilePath C:\Temp\gpresult.txt -Append
+
+
+
+cls
+
+Start-Transcript -Path "c:\temp\gpupdates.txt" -NoClobber
+foreach($computer in Get-Content -Path c:\temp\computers1.txt){
+    if(!(Test-Connection $computer -Count 1 -Quiet)) {
+       "$computer is offline"
+    }
+    else {
+        C:\temp\temp\tools\paexec.exe \\$computer cmd /C "netsh advfirewall firewall add rule dir=in name ="WMI" program=%systemroot%\system32\svchost.exe service=winmgmt action = allow protocol=TCP localport=any & call netsh firewall set service RemoteAdmin enable & netsh firewall add portopening protocol=tcp port=135 name=DCOM_TCP135"
+        }
+
+}
+#####################################################################
