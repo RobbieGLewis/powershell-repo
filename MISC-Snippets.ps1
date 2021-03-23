d47c4bbea48c3f4419c71f0194fc15070fc094ea
jameswylde <feruswylde@gmail.com>
2021-03-23 18:54:34 +0000

    .

--- MISC-Snippets.ps1
+++ MISC-Snippets.ps1
@@ -147,3 +147,17 @@ $clientName = Get-Content c:\temp\computers.txt
 C:\temp\temp\tools\paexec.exe \\$clientName cmd /C "netsh advfirewall firewall add rule dir=in name ="WMI" program=%systemroot%\system32\svchost.exe service=winmgmt action = allow protocol=TCP localport=any & call netsh firewall set service RemoteAdmin enable & netsh firewall add portopening protocol=tcp port=135 name=DCOM_TCP135" | Out-File -FilePath C:\Temp\gpresult.txt
 
 #Get-Content -Path C:\temp\TeamsRDPfix\result.txt
+
+
+
+
+
+foreach($computer in Get-Content -Path c:\temp\computers.txt){
+    if(-not (Test-Connection $computers -Count 1 -Quiet)) {
+        "Computer '$computer' is offline"
+    }
+    else {
+        C:\temp\temp\tools\paexec.exe \\$computer cmd /C "netsh advfirewall firewall add rule dir=in name ="WMI" program=%systemroot%\system32\svchost.exe service=winmgmt action = allow protocol=TCP localport=any & call netsh firewall set service RemoteAdmin enable & netsh firewall add portopening protocol=tcp port=135 name=DCOM_TCP135" | Out-File -FilePath C:\Temp\gpresult.txt -Append
+        }
+
+}
