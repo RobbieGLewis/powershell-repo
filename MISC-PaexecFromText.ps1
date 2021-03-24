4d49cbf7d446544f73521a6752e8c0f5a1f424cb
jameswylde <feruswylde@gmail.com>
2021-03-24 17:52:03 +0000

    .

--- MISC-PaexecFromText.ps1
+++ MISC-PaexecFromText.ps1
@@ -13,8 +13,8 @@ Rename-Item -Path c:\temp\export.csv -NewName "computers.txt"
 
 Clear-Host
 
-Start-Transcript -Path "c:\temp\gpupdates.txt" -NoClobber
-foreach($computer in Get-Content -Path c:\temp\computers.txt){
+Start-Transcript -Path "c:\temp\EXPORT-DONOTDELETE.txt" -NoClobber
+foreach($computer in Get-Content -Path c:\temp\computers(sweep2).txt){
     if(!(Test-Connection $computer -Count 1 -Quiet)) {
        "$computer"
     }
@@ -22,4 +22,27 @@ foreach($computer in Get-Content -Path c:\temp\computers.txt){
         C:\temp\temp\tools\paexec.exe \\$computer cmd /C "netsh advfirewall firewall add rule dir=in name ="WMI" program=%systemroot%\system32\svchost.exe service=winmgmt action = allow protocol=TCP localport=any & call netsh firewall set service RemoteAdmin enable & netsh firewall add portopening protocol=tcp port=135 name=DCOM_TCP135"
         }
 
+}
+
+
+
+#####
+#Loop
+
+Get-ADGroupMember "GBR-WS-DirectAccess" | Where-Object {$_Enabled -eq $false} | Export-Csv -path C:\temp\export1.csv
+Get-ADGroupMember "GBR-WS-DirectAccess" | Select-Object name | Export-Csv -path C:\temp\export.csv
+Rename-Item -Path c:\temp\export.csv -NewName "computers.txt"
+
+Clear-Host
+
+while($true){Start-Transcript -Path "c:\temp\EXPORT-DONOTDELETE.txt" -NoClobber
+foreach($computer in Get-Content -Path c:\temp\computers(sweep2).txt){
+    if(!(Test-Connection $computer -Count 1 -Quiet)) {
+       "$computer"
+    }
+    else {
+        C:\temp\temp\tools\paexec.exe \\$computer cmd /C "netsh advfirewall firewall add rule dir=in name ="WMI" program=%systemroot%\system32\svchost.exe service=winmgmt action = allow protocol=TCP localport=any & call netsh firewall set service RemoteAdmin enable & netsh firewall add portopening protocol=tcp port=135 name=DCOM_TCP135"
+        }
+
+}
 }
