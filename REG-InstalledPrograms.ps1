66911a9bc4469598dacbe3464a65a6bf9cbca54e
jameswylde <feruswylde@gmail.com>
2021-04-01 13:07:01 +0100

    .

---
+++ REG-InstalledPrograms.ps1
@@ -0,0 +1,1 @@
+Invoke-Command -ComputerName PC01 -ScriptBlock {get-Itemproperty HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\* | Select-Object Displayname, publisher, installdate} | Format-table -AutoSize
