27d4a47157e2a60f7b16161ca45c36f6785c27c3
jameswylde <feruswylde@gmail.com>
2021-04-06 18:27:47 +0100

    .

--- MISC-FindLargestFiles.ps1
+++ MISC-FindLargestFiles.ps1
@@ -16,12 +16,22 @@ Get-ChildItem -Recurse -ErrorAction SilentlyContinue | Sort-Object -Descending -
 
 Get-ChildItem -Recurse -ErrorAction SilentlyContinue | Sort-Object -Descending -Property Length | Select-Object -first 25 FullName, @{Name="Size (MB) ";Expression={[Math]::Round($_.length / 1MB, 2)}} | Export-CSV -Path C:\temp\largeFiles.csv
 
-#### Invoke-Command to particular machine and particular user share
+#### Invoke-Command to particular machine and particular user share and stored local c:\temp
 
 cls
 $clientName = Read-Host "Machine"
 $userName = Read-Host "User"
 Invoke-Command -ComputerName $clientName -ScriptBlock {Get-ChildItem c:\users\$using:userName\ -Recurse -ErrorAction SilentlyContinue | Sort-Object -Descending -Property Length | Select-Object -first 25 FullName, @{Name="Size (MB) ";Expression={[Math]::Round($_.length / 1MB, 2)}} | Export-CSV -NoTypeInformation -Path C:\temp\largeFiles.csv}
 
-### uses port 25 instead of 587
-Send-MailMessage -From 'james.wylde@smurfitkappa.co.uk' -To 'james.wylde@smurfitkappa.co.uk' -Subject 'Test email' -Body 'Test email' -Credential (Get-Credential) -SmtpServer 'mail.eu.smurfitkappa.com' -Port 25
+### Send via email with output to ts c:\
+
+Clear-Host
+$clientName = Read-Host "Machine"
+$userName = Read-Host "User"
+$recipientEmail = Read-Host "User Email"
+Invoke-Command -ComputerName $clientName -ScriptBlock {Get-ChildItem c:\users\$using:userName\ -Recurse -ErrorAction SilentlyContinue | Sort-Object -Descending -Property Length | Select-Object -first 25 FullName, @{Name="Size (MB) ";Expression={[Math]::Round($_.length / 1MB, 2)}}} | Export-CSV -NoTypeInformation -Path C:\temp\largeFiles.csv
+
+$outFile = "c:\temp\largeFiles.csv"
+$emailBody = Get-Content $outFile | Out-String
+
+Send-MailMessage -From 'james.wylde@smurfitkappa.co.uk' -To $recipientEmail -Subject 'Large Files' -Body -DeliveryNotificationOption OnSuccess, OnFailure $emailBody -Credential (Get-Credential "") -SmtpServer 'mail.eu.smurfitkappa.com' -Port 25
