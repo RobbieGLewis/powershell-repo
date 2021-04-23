fb9e0a8f8a2dc8ab1fa0606f5ecf7cb4e0a91874
jameswylde <feruswylde@gmail.com>
2021-04-23 10:34:03 +0100

    .

--- MISC-WindowsCreds.ps1
+++ MISC-WindowsCreds.ps1
@@ -1,1 +1,8 @@
-for /F "tokens=1,2 delims=" " %G in ('cmdkey /list ^| findstr Target') do cmdkey /delete %H
+for /F "tokens=1,2 delims=" %G in ('cmdkey /list ^| findstr Target') do cmdkey /delete %H
+
+for /F "tokens=1,* delims=" %G in ('cmdkey /list ^| findstr Target') do cmdkey /delete %H
+
+for /F "tokens=1,2 delims=" %G in ('cmdkey /list ^| findstr Onedrive*') do cmdkey /delete %H
+
+
+for /F 'tokens=1,2 delims=' %%G in ('cmdkey /list ^| findstr Onedrive') do cmdkey /delete %%H
