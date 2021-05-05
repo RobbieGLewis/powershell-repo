f7b5e7baea18df9ec412823cbda4d876484ee63e
jameswylde <feruswylde@gmail.com>
2021-05-05 11:32:41 +0100

    .

--- EXO-SmtpUpdate.ps1
+++ EXO-SmtpUpdate.ps1
@@ -9,3 +9,5 @@ Set-Mailbox "user" -EmailAddress "SMTP:name@email.com"
 Set-Mailbox "user" -PrimarySMTPAddress "SMTP:name@email.com"
 
 Set-Mailbox "user" -WindowsEmailAddress name@email.com
+
+Get-Mailbox -Identity "user" | Select-Object DisplayName,Alias,PrimarySMTPAddress,ForwardingAddress,ForwardingSMTPAddress
