66d9a0f039923098cf547e63ddbc98056fa414b5
jameswylde <feruswylde@gmail.com>
2021-05-05 12:48:41 +0100

    .

--- SCCM-SoftwareInstall.ps1
+++ SCCM-SoftwareInstall.ps1
@@ -1,8 +1,8 @@
 cls
 
 $clientName = Read-Host "Machine name"
-$appName = Read-Host "Exact SCCM Package Name"
-$installSwitch = Read-Host "Install or Uninstall"
+#$appName = Read-Host "Exact SCCM Package Name"
+#$installSwitch = Read-Host "Install or Uninstall"
 
 
 Function Trigger-AppInstallation
@@ -41,7 +41,17 @@ End {}
 }
 
 
-Trigger-AppInstallation -ComputerName $clientName -AppName "$appName" -Method $installSwitch
+#Trigger-AppInstallation -ComputerName $clientName -AppName "$appName" -Method $installSwitch
+
+Trigger-AppInstallation -ComputerName $clientName -AppName "SAP Logon For Windows_x86_7.60_ML" -Method Install
+
+Start-Sleep -S 5
+
+Trigger-AppInstallation -ComputerName $clientName -AppName "Analysis for Office x64 2.4.3.69599" -Method Install
+
+
+
+
 
 #Trigger-AppInstallation -ComputerName Uk-liv1-l27865 -AppName "NotepadPlusPlus_x64_7.9.3_ML" -Method Uninstall
 #Trigger-AppInstallation -ComputerName PC01 -AppName "Acrobat Reader DC_x64_2021.001.20142_ML" -Method Uninstall
