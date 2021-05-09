98d8ead43daa31cd17d4c42f1be69c6d149ed910
jameswylde <feruswylde@gmail.com>
2021-05-09 10:45:23 +0100

    .

--- SCCM-SoftwareInstall(LOOP).ps1
+++ SCCM-SoftwareInstall(LOOP).ps1
@@ -22,11 +22,47 @@ Invoke-CimMethod -Namespace ROOT\ccm\ClientSDK -ClassName CCM_Application -Compu
 
 $computer = Get-Content -Path c:\temp\sapcomputers.txt
 foreach ($computer in $computers) {
-    Invoke-Command -ComputerName $using:computer -ScriptBlock {
-        Trigger-AppInstallation -AppName "SAP Logon For Windows_x86_7.60_ML" -Method Install
-        Start-Sleep -S 30
-        Trigger-AppInstallation -AppName "Analysis for Office x64 2.4.3.69599" -Method Install
+
+    $appName = 'SAP Logon For Windows_x86_7.60_ML'
+
+    $Application = (Get-CimInstance -ClassName CCM_Application -Namespace "root\ccm\clientSDK" -ComputerName $Computername | Where-Object {$_.Name -like $AppName})
+ 
+    $Args = @{EnforcePreference = [UINT32] 0
+    Id = "$($Application.id)"
+    IsMachineTarget = $Application.IsMachineTarget
+    IsRebootIfNeeded = $False
+    Priority = 'High'
+    Revision = "$($Application.Revision)" }
+
+    $Instance = @(Get-CimInstance -ClassName CCM_Application -Namespace "root\ccm\clientSDK" -ComputerName $Computername | Where-Object {$_.Name -like $AppName})
+    Invoke-CimMethod -Namespace ROOT\ccm\ClientSDK -ClassName CCM_Application -ComputerName $computerNAME -MethodName Install -Arguments $Args
+
+
     }
 }
+
+
+
+
+####
+
+
+$computerName = Get-Content C:\temp\sapcomputers.txt
+
+$appName = 'SAP Logon For Windows_x86_7.60_ML'
+
+$Application = (Get-CimInstance -ClassName CCM_Application -Namespace "root\ccm\clientSDK" -ComputerName $Computername | Where-Object {$_.Name -like $AppName})
+
+$Args = @{EnforcePreference = [UINT32] 0
+Id = "$($Application.id)"
+IsMachineTarget = $Application.IsMachineTarget
+IsRebootIfNeeded = $False
+Priority = 'High'
+Revision = "$($Application.Revision)" }
+
+$Instance = @(Get-CimInstance -ClassName CCM_Application -Namespace "root\ccm\clientSDK" -ComputerName $Computername | Where-Object {$_.Name -like $AppName})
+Invoke-CimMethod -Namespace ROOT\ccm\ClientSDK -ClassName CCM_Application -ComputerName $computerNAME -MethodName Install -Arguments $Args | ft -AutoSize
+
+
 
 
