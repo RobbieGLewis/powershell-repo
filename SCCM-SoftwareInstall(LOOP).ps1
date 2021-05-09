b9297dd72adeae9cbf4bc3c307a1def4e0b1f98e
jameswylde <feruswylde@gmail.com>
2021-05-09 14:43:35 +0100

    .

--- SCCM-SoftwareInstall(LOOP).ps1
+++ SCCM-SoftwareInstall(LOOP).ps1
@@ -189,3 +189,45 @@ foreach ($computer in $computers) {
 }
 }
 catch{}
+
+
+
+################ final
+
+
+$ErrorActionPreference = 'SilentlyContinue'
+
+CLS
+
+Start-Transcript -Path c:\temp\SAP760progress.txt -Append
+
+$computers = Get-Content -Path c:\temp\sapcomputers.txt
+try{
+    foreach ($computer in $computers) {
+        if(!(Test-Connection $computer -Count 1 -Quiet)) {
+            "$computer is offline/unavailable."
+        }
+
+        else{
+
+            C:\windows\paexec.exe paexec.exe \\$computer  -s winrm.cmd quickconfig -q
+
+            $appName = 'SAP Logon For Windows_x86_7.60_ML'
+
+            $Application = (Get-CimInstance -ClassName CCM_Application -Namespace "root\ccm\clientSDK" -ComputerName $computer | Where-Object {$_.Name -like $AppName})
+
+            $ccmArgs = @{EnforcePreference = [UINT32] 0
+            Id = "$($Application.id)"
+            IsMachineTarget = $Application.IsMachineTarget
+            IsRebootIfNeeded = $False
+            Priority = 'High'
+            Revision = "$($Application.Revision)" }
+
+            $Instance = @(Get-CimInstance -ClassName CCM_Application -Namespace "root\ccm\clientSDK" -ComputerName $Computer | Where-Object {$_.Name -like $AppName})
+            Invoke-CimMethod -Namespace ROOT\ccm\ClientSDK -ClassName CCM_Application -ComputerName $computer -MethodName Install -Arguments $ccmArgs | ft -AutoSize
+
+            }
+    
+}
+}
+catch{}
