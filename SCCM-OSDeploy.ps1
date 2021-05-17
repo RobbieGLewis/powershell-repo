03ae0ef562a12f39e10fa468c62264e7049d45d1
jameswylde <feruswylde@gmail.com>
2021-05-17 17:05:41 +0100

    .

--- SCCM-OSDeploy.ps1
+++ SCCM-OSDeploy.ps1
@@ -1,64 +1,5 @@
 #   James Wylde
 
-cls
-
-$computerName = Read-Host "Machine"
-$taskName = Read-Host "Update"
-
-Get-Service -Name winrm -ComputerName $computername | Set-Service -Status Running
-
-
-$SoftwareDistributionPolicy = Get-WmiObject -Namespace "root\ccm\policy\machine\actualconfig" -Class "CCM_SoftwareDistribution" -ComputerName $computername | Where-Object { $_.PKG_Name -like $taskName } | Select-Object -Property PKG_Name, PKG_PackageID, ADV_AdvertisementID
-$ScheduleID = Get-WmiObject -Namespace "root\ccm\scheduler" -Class "CCM_Scheduler_History" -ComputerName $computerName | Where-Object { $_.ScheduleID -like "*$($SoftwareDistributionPolicy.PKG_PackageID)*" } | Select-Object -ExpandProperty ScheduleID
-
-$TaskSequencePolicy = Get-WmiObject -Namespace "root\ccm\policy\machine\actualconfig" -Class "CCM_TaskSequence" -ComputerName $computerName  | Where-Object { $_.ADV_AdvertisementID -like $SoftwareDistributionPolicy.ADV_AdvertisementID }
-if ($TaskSequencePolicy.ADV_RepeatRunBehavior -notlike "RerunAlways") {
-    $TaskSequencePolicy.ADV_RepeatRunBehavior = "RerunAlways"
-    $TaskSequencePolicy.Put() | Out-Null
-}
-
-$TaskSequencePolicy.Get()
-$TaskSequencePolicy.ADV_MandatoryAssignments = $true
-$TaskSequencePolicy.Put() | Out-Null
-
-Invoke-WmiMethod  -ComputerName $computername -Namespace "root\ccm" -Class "SMS_Client" -Name "TriggerSchedule" -ArgumentList $ScheduleID
-
-
-
-
-
-
-
-####
-
-
-cls
-
-$computer = Read-Host "Machine name"
-
-
-$TaskSequenceName = "Deploy COW10 - Production - Win10 20H2 Upgrade"
-$SoftwareDistributionPolicy = Get-WmiObject -Namespace "root\ccm\policy\machine\actualconfig" -Class "CCM_SoftwareDistribution" -ComputerName $computer | Where-Object { $_.PKG_Name -like $TaskSequenceName } | Select-Object -Property PKG_Name, PKG_PackageID, ADV_AdvertisementID
-$ScheduleID = Get-WmiObject -Namespace "root\ccm\scheduler" -Class "CCM_Scheduler_History" -ComputerName $computer | Where-Object { $_.ScheduleID -like "*$($SoftwareDistributionPolicy.PKG_PackageID)*" } | Select-Object -ExpandProperty ScheduleID
-
-$TaskSequencePolicy = Get-WmiObject -Namespace "root\ccm\policy\machine\actualconfig" -Class "CCM_TaskSequence" -ComputerName $computer  | Where-Object { $_.ADV_AdvertisementID -like $SoftwareDistributionPolicy.ADV_AdvertisementID }
-if ($TaskSequencePolicy.ADV_RepeatRunBehavior -notlike "RerunAlways") {
-    $TaskSequencePolicy.ADV_RepeatRunBehavior = "RerunAlways"
-    $TaskSequencePolicy.Put() | Out-Null
-}
-
-$TaskSequencePolicy.Get()
-$TaskSequencePolicy.ADV_MandatoryAssignments = $true
-$TaskSequencePolicy.Put() | Out-Null
-
-Invoke-WmiMethod  -ComputerName $computer -Namespace "root\ccm" -Class "SMS_Client" -Name "TriggerSchedule" -ArgumentList $ScheduleID
-
-
-
-
-#####
-
-
 #----------------------------------------------------------------------------------------#
 
 $ErrorActionPreference = 'SilentlyContinue'
@@ -67,13 +8,13 @@ $ErrorActionPreference = 'SilentlyContinue'
 
 CLS
 
-Start-Transcript -Path c:\temp\SAP760progress.txt -Append
+Start-Transcript -Path c:\temp\20h2progress.txt -Append
 
-$computers = Get-Content -Path c:\temp\sapcomputers.txt
+$computers = Get-Content -Path c:\temp\computers.txt
 try{
     foreach ($computer in $computers) {
         if(!(Test-Connection $computer -Count 1 -Quiet)) {
-            "$computer                                                                           Failure"
+            "$computer                                                                                                                 Failure"
         }
 
         else{
@@ -94,7 +35,8 @@ try{
             $TaskSequencePolicy.ADV_MandatoryAssignments = $true
             $TaskSequencePolicy.Put() | Out-Null
             
-            Invoke-WmiMethod  -ComputerName $computer -Namespace "root\ccm" -Class "SMS_Client" -Name "TriggerSchedule" -ArgumentList $ScheduleID
+            Invoke-WmiMethod  -ComputerName $computer -Namespace "root\ccm" -Class "SMS_Client" -Name "TriggerSchedule" -ArgumentList $ScheduleID |
+            Select-Object @{Name = 'Computer Name'; Expression = {$_.PSComputerName}},@{Name = 'Result'; Expression = { if($_.ReturnValue -eq ' ') {'Success'} else {'Success'}}} 
             
 
         }
