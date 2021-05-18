e81582c5c45fed3c756838a1a95a4b696c99419e
jameswylde <feruswylde@gmail.com>
2021-05-18 15:15:00 +0100

    .

--- AD-FindUserMatches.ps1
+++ AD-FindUserMatches.ps1
@@ -4,6 +4,6 @@
 #   Modules
 Import-Module ActiveDirectory
 
-Get-ADUser -Filter {description -eq 'General Manager' -and country -eq 'GB'} -Properties description, country | Select-Object name, samaccountname, country | ft -AutoSize
+Get-ADUser -Filter {description -eq 'General Manager' -and country -eq 'GB' -and enabled -eq $true} -Properties description, country | Select-Object name, samaccountname, country | ft -AutoSize
 
-Get-ADUser -Filter {description -eq 'Financial Controller' -and country -eq 'GB'} -Properties description, country | Select-Object name, samaccountname, country | ft -AutoSize
+Get-ADUser -Filter {description -eq 'Financial Controller' -and country -eq 'GB' -and enabled -eq $true} -Properties description, country | Select-Object name, samaccountname, country | ft -AutoSize
