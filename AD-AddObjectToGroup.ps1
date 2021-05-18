7730186474bdd2a739b7b394df477e06cb26a620
jameswylde <feruswylde@gmail.com>
2021-05-18 13:20:55 +0100

    .

---
+++ AD-AddObjectToGroup.ps1
@@ -0,0 +1,16 @@
+#   James Wylde
+
+#----------------------------------------------------------------------------------------#
+#   Modules
+Import-Module ActiveDirectory
+
+$computers = Get-Content c:\temp\computers.csv
+
+foreach ($computer in $computers) { 
+    $compAdd = Get-ADComputer $computer
+    Add-ADGroupMember -ID GBR-SD-SCCM-CAS002E3-MA -Members $compAdd
+
+}
+
+
+#Remove-ADGroupMember
