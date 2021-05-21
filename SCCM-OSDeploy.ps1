8a602ee7cffeec75ce55931a3d2cd15321f90cf3
jameswylde <feruswylde@gmail.com>
2021-05-21 10:08:14 +0100

    .

--- SCCM-OSDeploy.ps1
+++ SCCM-OSDeploy.ps1
@@ -43,3 +43,42 @@ $computers = Get-Content -Path c:\temp\computers.txt
     
 }
 
+
+
+
+########
+
+
+
+#----------------------------------------------------------------------------------------#
+
+CLS
+
+Start-Transcript -Path c:\temp\20h2progress.txt -Append
+
+$computers = Get-Content -Path c:\temp\computers.txt
+
+    foreach ($computer in $computers) {
+        if(!(Test-Connection $computer -Count 1 -Quiet)) {
+            "$computer                                                                                                     Failure"
+        }
+
+        else{
+
+            C:\windows\paexec.exe paexec.exe \\$computer  -s winrm.cmd quickconfig -q
+
+            $TaskSequenceName = "Deploy COW10 - Production - Win10 20H2 Upgrade"
+            $SoftwareDistributionPolicy = Get-WmiObject -Namespace "root\ccm\policy\machine\actualconfig" -Class "CCM_SoftwareDistribution" -ComputerName $computer | Where-Object { $_.PKG_Name -like $TaskSequenceName } | Select-Object -Property PKG_Name, PKG_PackageID, ADV_AdvertisementID
+            $ScheduleID = Get-WmiObject -Namespace "root\ccm\scheduler" -Class "CCM_Scheduler_History" -ComputerName $computer | Where-Object { $_.ScheduleID -like "*$($SoftwareDistributionPolicy.PKG_PackageID)*" } | Select-Object -ExpandProperty ScheduleID
+           
+            
+            Invoke-WmiMethod  -ComputerName $computer -Namespace "root\ccm" -Class "SMS_Client" -Name "TriggerSchedule" -ArgumentList $ScheduleID |
+            Select-Object @{Name = 'Computer Name'; Expression = {$_.PSComputerName}},@{Name = 'Result'; Expression = { if($_.__GENUS -eq '1') {'Success'} else {'Failure'}}} 
+            
+
+        }
+    
+}
+
+
+
