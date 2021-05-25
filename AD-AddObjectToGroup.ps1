4ae53d51ea5e3d811ac40885557ca9e87d263f63
jameswylde <feruswylde@gmail.com>
2021-05-25 14:22:41 +0100

    .

--- AD-AddObjectToGroup.ps1
+++ AD-AddObjectToGroup.ps1
@@ -12,16 +12,27 @@ foreach ($computer in $computers) {
 
 }
 
-
+#----------------------------------------------------------------------------------------#
 
 Import-Module ActiveDirectory
 
 $users = Get-ADuser c:\temp\visionusers.txt
+$destinationGroup = "GBR-UK-HUB-U-Vision_Users"
+
+$startCount = (Get-ADGroupMember -Identity $destinationGroup).Count
+$newMembersCount = $users.Count
 
 foreach ($user in $users) { 
     $userAdd = Get-ADUser $user
-    Add-ADGroupMember -ID "GBR-UK-HUB-U-Vision_Users" -Members $userAdd -PassThru
+    Add-ADGroupMember -ID $destinationGroup -Members $userAdd -PassThru
 
 }
+
+$endCount = (Get-ADGroupMember -Identity $destinationGroup).Count
+
+$newCount = $endCount - $startCount
+
+Write-Host - "$newMembersCount users have been added to group $destinationGroup" -ForegroundColor White -BackgroundColor Green
+Write-Host - "$newCount NET TOTAL + $destinationGroup" -ForegroundColor White -BackgroundColor Green
 
 #Remove-ADGroupMember
