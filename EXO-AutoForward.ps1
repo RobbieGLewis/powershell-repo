83f6c98e181d2890c29d0b8014fc413c904a1734
jameswylde <feruswylde@gmail.com>
2021-05-25 11:23:25 +0100

    .

--- EXO-AutoForward.ps1
+++ EXO-AutoForward.ps1
@@ -6,3 +6,5 @@
 Set-Mailbox -Identity "person@email.com" -DeliverToMailboxAndForward $true -ForwardingAddress "person2@email.com"
 
 Set-Mailbox -Identity "person@email.com" ForwardingAddress "person2@email.com"
+
+Get-Mailbox -Identity "person@email.com" | Format-List ForwardingAddress,DeliverToMailboxAndForward
