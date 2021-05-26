cf715bb97b4786981180a679426fd93c403811b9
jameswylde <feruswylde@gmail.com>
2021-05-26 12:16:32 +0100

    .

--- SCCM-ProvisioningMode.ps1
+++ SCCM-ProvisioningMode.ps1
@@ -6,3 +6,11 @@
 Invoke-WmiMethod -Namespace root\CCM -Class SMS_Client -Name SetClientProvisioningMode -ArgumentList $false
 
 HKLM\SOFTWARE\Microsoft\CCM\CcmExec ProvisioningMode False
+
+
+###
+
+
+sc config smstsmgr demand= winmgmt/ccmexec
+sc config smstsmgr start= demand
+net start smstsmgr
