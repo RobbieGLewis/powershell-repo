ee5118d0380fdb51935112b36c6e1368a9584721
jameswylde <feruswylde@gmail.com>
2021-06-01 16:43:44 +0100

    .

---
+++ REG-AutoHideTaskbar.ps1
@@ -0,0 +1,6 @@
+#   James Wylde
+
+#----------------------------------------------------------------------------------------#
+#   Modules
+
+reg add "HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\StuckRects3" /v Settings /T REG_BINARY /d "0300000000000F0EFF0F0FFF000302000000000300000000030E00000000020800000000000000000000050C030000000A0005000000080403000000060000000000000100000000" /f
--- REG-DelayedStart.ps1
+++ REG-DelayedStart.ps1
@@ -15,21 +15,3 @@ C:\temp\TeamsRDPfix\tools\paexec.exe \\$clientName cmd /C "reg.exe Add "HKEY_LOC
 
 
 reg.exe Add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\NlaSvc" /v Start /T REG_SZ /d 2 /F"
-
-
-
-
-reg.exe Add "HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\StuckRects3" /v Settings /T REG_BINARY /d '30 00 00 00 fe ff ff ff 03 02 00 00 03 00 00 00 3e 00 00 00 28 00 00 00 00 00 00 00 5c 03 00 00 a0 05 00 00 84 03 00 00 60 00 00 00 01 00 00 00' /f
-
-
-reg Add "HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\StuckRects3" /v Settings /T REG_BINARY /d "30 00 00 00 fe ff ff ff 03 02 00 00 03 00 00 00 3e 00 00 00 28 00 00 00 00 00 00 00 5c 03 00 00 a0 05 00 00 84 03 00 00 60 00 00 00 01 00 00 00" /f
-
-
-
-
-'30,00,00,00,fe,ff,ff,ff,02,20,00,00,03,00,00,00,3e,00,00,00,28,\,00,00,00,00,00,00,00,10,04,00,00,80,07,00,00,38,04,00,00,60,00,00,00,01,00,\,00,00'
-
-
-reg Add "HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\StuckRects3" /v Settings /T REG_BINARY /d "hex:30 00 00 00 fe ff ff ff 02 20 00 00 03 00 00 00 3e 00 00 00 28 00 00 00 00 00 00 00 10 04 00 00 80 07 00 00 38 04 00 00 60 00 00 00 01 00 00 00" /f
-
-powershell.exe -command "$RegValues = (Get-ItemProperty -Path "dir HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\StuckRects3").Settings | Set-ItemProperty -Path "dir HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\StuckRects3" -Name -Value 3"
