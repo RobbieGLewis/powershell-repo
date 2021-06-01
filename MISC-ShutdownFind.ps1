234138223c02c21c6479f20694cab64e205a6820
jameswylde <feruswylde@gmail.com>
2021-06-01 13:06:32 +0100

    .

--- MISC-ShutdownFind.ps1
+++ MISC-ShutdownFind.ps1
@@ -2,13 +2,14 @@ $compName = Read-Host "Client Name"
 
 cls
 
-Write-Host "Who killed" $compName"?" -ForegroundColor White -BackgroundColor Black
+Write-Host "Who killed" $compName"?" -ForegroundColor White -BackgroundColor Red
 
 $properties = @(
     @{n='When';e={$_.TimeCreated}},
     @{n='Who';e={$_.Properties[6].Value}},
-    @{n='What/Why';e={$_.Properties[0].Value}},
-    @{n='How';e={$_.Properties[4].Value.ToUpper()}} 
+    @{n='How';e={$_.Properties[4].Value.ToUpper()}},
+    @{n='What/Why';e={$_.Properties[0].Value}}
+    
 )
 Get-WinEvent -ComputerName $compName -FilterHashTable @{LogName='System'; ID=1074} | 
-Select-Object $properties | Sort-Object "$_.TimeCreated" | Format-Table -AutoSize
+Select-Object $properties | Sort-Object "$_.TimeCreated" -Descending | Format-Table -AutoSize
