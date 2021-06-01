2d7c71cde6c6f8aedcf26be5feeda97d3a953360
jameswylde <feruswylde@gmail.com>
2021-06-01 14:05:11 +0100

    .

--- MISC-ShutdownFind.ps1
+++ MISC-ShutdownFind.ps1
@@ -3,6 +3,7 @@
 #----------------------------------------------------------------------------------------#
 #   Modules
 
+cls
 
 $compName = Read-Host "Client Name"
 
@@ -15,11 +16,16 @@ $properties = @(
     @{n='Who';e={$_.properties[6].Value.ToUpper()}},
     @{n='How';e={$_.properties[4].Value.ToUpper()}},
     @{n='What/Why';e={$_.properties[0].Value}}
-    
 )
+
 Get-WinEvent -ComputerName $compName -FilterHashTable @{LogName='System'; ID=1074} | 
 Select-Object $properties | Sort-Object "$_.timeCreated" -Descending | Format-Table -AutoSize
 
+Start-Sleep -Seconds 1.5
+
+$timeUp =  SystemInfo /s $compName /fo list | find /i "Boot Time:" 
+
+Write-Host "" $timeUp -ForegroundColor White -BackgroundColor Red
 
 #Get-WinEvent -ComputerName $compName -FilterHashTable @{LogName='System'; ID=1074} | 
 #Select-Object $properties | Sort-Object "$_.timeCreated" -Descending | Out-GridView
