83d562e8b535f833729c188098b448c0f0ec9afc
jameswylde <feruswylde@gmail.com>
2021-06-03 15:08:50 +0100

    .

--- REG-InstalledPrograms.ps1
+++ REG-InstalledPrograms.ps1
@@ -1,3 +1,22 @@
 #   James Wylde
 
-Invoke-Command -ComputerName PC01 -ScriptBlock {get-Itemproperty HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\* | Select-Object Displayname, publisher, installdate} | Format-table -AutoSize
+Invoke-Command -ComputerName UK-LIV1-L023572 -ScriptBlock {get-Itemproperty HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\* | Select-Object Displayname, publisher, installdate} | Format-table -AutoSize
+
+
+
+
+
+
+# Loop through each entry under Products   
+Get-ChildItem Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Installer\UserData\S-1-5-18\Products -Recurse -Name | ForEach-Object {   
+  
+# Find your software by its display name   
+if ((Get-ItemProperty Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Installer\UserData\S-1-5-18\Products\$_\InstallProperties -Name DisplayName -ErrorAction SilentlyContinue).DisplayName -eq "SAP") {   
+  
+# Grab the Uninstall String while removing "msiexec /i" from the string   
+$UninstallString = (Get-ItemProperty Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Installer\UserData\S-1-5-18\Products\$_\InstallProperties).UninstallString -replace "MsiExec.exe /I", "SAP"   
+}   
+}   
+  
+# Uninstall software   
+Start-Process MsiExec.exe -ArgumentList "/x $UninstallString /passive" -Wait
