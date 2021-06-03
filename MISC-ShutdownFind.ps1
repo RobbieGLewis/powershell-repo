c08a33686d928b3f7358ff0b303541f1b3d6373d
jameswylde <feruswylde@gmail.com>
2021-06-03 12:20:09 +0100

    .

--- MISC-ShutdownFind.ps1
+++ MISC-ShutdownFind.ps1
@@ -36,7 +36,7 @@ CATCH [Exception]
     if ($_.Exception.GetType().Name -ne "RPC")
     {
         Write-Host "`n"
-        Write-Host "We may never find who killed $compName as the machine isn't reachable (RPC is unavailable) - try locally or try 'net start winrm'" -ForegroundColor White -BackgroundColor Red
+        Write-Host " $compName isn't reachable (RPC is unavailable) - try locally instead." -ForegroundColor White -BackgroundColor Red
         Write-Host "`n"
     }
 
