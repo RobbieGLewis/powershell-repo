b8c1c8b4920fda6be5bba6d60f84c47f0c1760f5
jameswylde <feruswylde@gmail.com>
2021-06-10 14:24:34 +0100

    .

---
+++ MISC-SetWindowsEnvVars.ps1
@@ -0,0 +1,8 @@
+# User
+
+setx ps "C:\Users\wyldeja\OneDrive - Smurfit Kappa\Documents\admin\github\powershell-repo"
+
+
+# System
+
+setx ps "C:\Users\wyldeja\OneDrive - Smurfit Kappa\Documents\admin\github\powershell-repo" /M
