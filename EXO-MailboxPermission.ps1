410547f66e03cfc6730fcf928bd1e930ef36d8b1
jameswylde <feruswylde@gmail.com>
2021-06-28 13:36:50 +0100

    .

--- EXO-MailboxPermission.ps1
+++ EXO-MailboxPermission.ps1
@@ -4,3 +4,5 @@
 #   Modules
 
 Add-MailboxPermission "resource.to.access@email.com" -user "person.wanting.access@email.com" -AccessRights fullaccess -Confirm:$false
+
+Add-RecipientPermission "shared.mailbox@email.com" -AccessRights SendAs -Trustee "person.wanting.access@email.com"
