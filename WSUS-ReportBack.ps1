e39413234108fdf9614ce5a3c0ba722e02d4c40b
jameswylde <feruswylde@gmail.com>
2021-07-01 13:46:51 +0100

    .

--- WSUS-ReportBack.ps1
+++ WSUS-ReportBack.ps1
@@ -167,3 +167,25 @@ Windows Registry Editor Version 5.00
 "ScheduledInstallTime"=dword:0000000a
 "ScheduledInstallEveryWeek"=dword:00000001
 
+
+#########################
+
+@ECHO OFF
+ echo Simple Script to Reset / Clear Windows Update
+ echo.
+
+ echo.
+ attrib -h -r -s %windir%\system32\catroot2
+ attrib -h -r -s %windir%\system32\catroot2\*.*
+ net stop wuauserv
+ net stop CryptSvc
+ net stop BITS
+ ren %windir%\system32\catroot2 catroot2.old
+ ren %windir%\SoftwareDistribution sold.old
+ ren "%ALLUSERSPROFILE%\application data\Microsoft\Network\downloader" downloader.old
+ net Start BITS
+ net start CryptSvc
+ net start wuauserv
+ echo.
+ echo Task completed successfully...
+ echo.
