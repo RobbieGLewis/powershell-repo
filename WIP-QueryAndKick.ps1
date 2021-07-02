2c64731133e7fd5312ea63b257294c26503cfa06
jameswylde <feruswylde@gmail.com>
2021-07-02 13:22:25 +0100

    .

--- WIP-QueryAndKick.ps1
+++
@@ -1,5 +0,0 @@
-
-
-qwinsta /server:UK-HUB3-M1209 && qwinsta /server:UK-HUB3-M1208
-
-logoff /server:UK-HUB3-M1215 150 /v
---
+++ WIP-Scraps.ps1
@@ -0,0 +1,9 @@
+
+
+qwinsta /server:UK-HUB3-M1209 && qwinsta /server:UK-HUB3-M1208
+
+logoff /server:UK-HUB3-M1215 150 /v
+
+control userpasswords2
+
+net user Administrator 
