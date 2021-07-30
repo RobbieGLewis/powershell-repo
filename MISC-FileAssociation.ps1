870fc799bb8d058eca6995f41f5206c7c1764619
jameswylde <feruswylde@gmail.com>
2021-07-30 16:15:34 +0100

    .

--- MISC-FileAssociation.ps1
+++ MISC-FileAssociation.ps1
@@ -1,2 +1,2 @@
-assoc .sapao="C:\Program Files\SAP BusinessObjects\Office AddIn\BiOfficeLauncher.exe"
+assoc .sapaox="C:\Program Files\SAP BusinessObjects\Office AddIn\BiOfficeLauncher.exe"
 assoc .pdf="C:\Program Files (x86)\Adobe\Acrobat Reader DC\Reader\AcroRd32.exe"
