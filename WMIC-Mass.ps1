92b4a87749b6eff55337828f9c36bef773fbf063
jameswylde <feruswylde@gmail.com>
2021-08-06 09:13:20 +0100

    .

--- WMIC-Mass.ps1
+++ WMIC-Mass.ps1
@@ -7,3 +7,5 @@ product where version="7.5.1.2" call uninstall /y
 product where name="8x8 Work"
 
 wmic product where name="Cofense Reporter" call uninstall /y
+
+wmic product where name="cape pack v2.15" call uninstall /y
