02c39de7e3b3046affbc94af5682cb4e92bf6a65
jameswylde <feruswylde@gmail.com>
2021-08-24 16:40:11 +0100

    .

--- AD-ExportGroupCSV.ps1
+++ AD-ExportGroupCSV.ps1
@@ -7,10 +7,9 @@ Import-Module ActiveDirectory
 # Enabled
 #Get-ADUser -Properties * -Filter {Enabled -eq 'True'} | Where-Object {($_.memberof -like "_UK DSS ALL")} | Select-Object @{Label = "Email";Expression = {$_.EmailAddress}}, @{Label = "First Name";Expression = {$_.GivenName}}, @{Label = "Last Name";Expression = {$_.Surname}}, @{Label = "Group";Expression = {($_.canonicalname -Split "/")[-2]}} | Export-Csv -Path c:\temp\export2.csv -NoTypeInformation
 
-# Get-ADGroupMember -identity "_UK DSS All Users" | select name | Export-Csv -path C:\temp\export.csv
 
 
-Get-ADGroupMember "_UK DSS All Users" | ForEach-Object  {get-aduser $_ -properties emailaddress} | Select-Object Name, SamAccountName, EMailaddress | Export-Csv -path C:\temp\export2.csv
+Get-ADGroupMember "group" | ForEach-Object  {Get-ADUser $_ -properties emailaddress} | Select-Object Name, SamAccountName, EmailAddress | Export-Csv -path C:\temp\ADGroupExport.csv
 
 
-Get-ADGroupMember "GBR-WS-DirectAccess" | Select-Object name | Export-Csv -path C:\temp\export2.csv
+Get-ADGroupMember "group" | Select-Object name | Export-Csv -path C:\temp\export2.csv
