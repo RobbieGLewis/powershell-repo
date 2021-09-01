88ec8a2107680b2bd6a993e3096ffb326ac6052c
jameswylde <feruswylde@gmail.com>
2021-09-01 12:03:43 +0100

    .

--- WSUS-List.ps1
+++ WSUS-List.ps1
@@ -5,3 +5,17 @@ $computers = get-content C:\Users\a1-wyldeja\Desktop\wsus.txt
 foreach ($computer in $computers){
 
 get-wsuscomputer -NameIncludes $computer -ErrorAction stop}
+
+
+### Get enabled
+
+$ErrorActionPreference = "SilentlyContinue"
+$computers = get-content C:\temp\wsus.txt
+
+foreach ($computer in $computers){
+
+  $status = Get-ADComputer -Identity $computer -Properties * | select enabled -ErrorAction SilentlyContinue
+  if (($Status | Select -ExpandProperty enabled) -eq $true )
+    {Write-Host $computer}
+
+}
