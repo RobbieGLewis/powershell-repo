13dd481f9998ae75cf4ee50c499438a82506c643
jameswylde <feruswylde@gmail.com>
2021-09-09 13:24:47 +0100

    .

--- REG-AutoHideTaskbar.ps1
+++
@@ -1,6 +0,0 @@
-#   James Wylde
-
-#----------------------------------------------------------------------------------------#
-#   Modules
-
-reg add "HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\StuckRects3" /v Settings /T REG_BINARY /d "30000000FEFFFFFF03020000030000003E00000028000000000000001004000080070000380400006000000001000000" /f
